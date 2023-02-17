1361e0bdee6b39a2bac66d5399099543382b4d81
FatimaMuhammadAdam <98468845+FatimaMuhammadAdam@users.noreply.github.com>
2023-02-17 14:43:39 +0100

    Update chapter_9_excersise.py

--- Project/chapter_9/chapter_9_excersise.py
+++ Project/chapter_9/chapter_9_excersise.py
@@ -150,3 +150,32 @@ user.reset_login_attempts()
 
 # Printing the value of login_attempts to make sure it was reset to 0
 print(f"Number of login attempts after reset: {user.login_attempts}")
+
+
+#9-6. Ice Cream Stand: An ice cream stand is a specific kind of restaurant. Write 
+#a class called IceCreamStand that inherits from the Restaurant class you wrote 
+#in Exercise 9-1 (page 162) or Exercise 9-4 (page 167). Either version of 
+#the class will work; just pick the one you like better. Add an attribute called 
+#flavors that stores a list of ice cream flavors. Write a method that displays 
+#these flavors. Create an instance of IceCreamStand, and call this method.
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} serves {self.cuisine_type} cuisine.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type, flavors):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = flavors
+    def display_flavours(self):
+        print("In our restuarant we sell icecream and the icecream has the following flavours")
+        for flavor in self.flavors:
+            print(f"\t- {flavor}")
+my_icecream = IceCreamStand( 'icecream', ['chocolate flavor','banana flavor', 'vanilla flavor'])
+print(my_icecream.describe_restaurant())
+print(my_icecream.display_flavours())
