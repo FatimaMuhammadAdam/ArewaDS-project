b5520c99fa59256aacb8d733ca2aefec923d48d9
FatimaMuhammadAdam <98468845+FatimaMuhammadAdam@users.noreply.github.com>
2023-02-17 15:27:40 +0100

    Update chapter_9_excersise.py

--- Project/chapter_9/chapter_9_excersise.py
+++ Project/chapter_9/chapter_9_excersise.py
@@ -215,3 +215,24 @@ class Admin(User):
 
 admin = Admin("Fatima", "Muhammad Adam", 25, "Dutse", "Federal Universisty Dutse")
 admin.show_privileges()
+
+# Write a separate Privileges class. The class should have one 
+#attribute, privileges, that stores a list of strings as described in Exercise 9-7. 
+#Move the show_privileges() method to this class. Make a Privileges instance 
+#as an attribute in the Admin class. Create a new instance of Admin and use your 
+#method to show its privileges.
+class Privileges:
+    def __init__(self):
+        self.privileges = ["admin can add post", "admin can delete post", "admin can ban user"]
+        
+    def show_privileges(self):
+        print("The privileges that the admins has are as follows:")
+        for privilege in self.privileges:
+            print("- " + privilege)
+
+class Admin:
+    def __init__(self, name):
+        self.name = name
+        self.privileges = Privileges()
+admin = Admin("Fatima Muhammad Adam")
+admin.privileges.show_privileges()
