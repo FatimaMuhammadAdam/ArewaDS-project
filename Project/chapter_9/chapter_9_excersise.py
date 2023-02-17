c49ebee7212ee36467235563a6798239443f30ae
FatimaMuhammadAdam <98468845+FatimaMuhammadAdam@users.noreply.github.com>
2023-02-17 12:02:56 +0100

    Update chapter_9_excersise.py

--- Project/chapter_9/chapter_9_excersise.py
+++ Project/chapter_9/chapter_9_excersise.py
@@ -66,3 +66,39 @@ user2.greet_user()
 
 user3.describe_user()
 user3.greet_user()
+
+#9-4. Number Served: Start with your program from Exercise 9-1 (page 162). 
+#Add an attribute called number_served with a default value of 0. Create an 
+#instance called restaurant from this class. Print the number of customers the 
+#restaurant has served, and then change this value and print it again.
+#Add a method called set_number_served() that lets you set the number 
+#of customers that have been served. Call this method with a new number and 
+#print the value again.
+#Add a method called increment_number_served() that lets you increment 
+#the number of customers who’ve been served. Call this method with any number 
+# you like that could represent how many customers were served in, say, a day of business.
+
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self. number_served =0
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} serves {self.cuisine_type} cuisine.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+    def set_number_served(self, number):
+        self.number_served = number
+    def increment_number_served(self, number):
+        self.number_served +=  number
+
+
+restaurant = Restaurant("Hasina Cetaring", "Nigerian")
+print(f'the resturant has served {restaurant.number_served} customers')
+restaurant.number_served = 12
+print(f"The restaurant has served {restaurant.number_served} customers.")
+restaurant.number_served = 16
+print(f"The restaurant has served {restaurant.number_served} customers.")
+restaurant.number_served = 20
+print(f"The restaurant has served {restaurant.number_served} customers.")
